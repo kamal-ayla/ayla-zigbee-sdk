82c4c8ccace59b4fa18da90956031accdaf90946
Maciej Andrzejewski <maciej.andrzejewski@digica.com>
2023-09-19 14:18:56 +0200

    Create a standalone build script.

--- kvsd_apps/kvsd_build_apps.sh
+++ kvsd_apps/kvsd_build_apps.sh
@@ -1,83 +1,115 @@
 #!/bin/bash
 
 help() {
-    echo "Usage: $0 <build_dir> <kvsd_src_dir> <cmake_defines>"
+    echo "Usage: $0 <cmake_c_flags_path> <cmake_toolchain_flags_path> <cmake_library_path>"
     exit 1
 }
 
 install_hls_sdk() {
-	cd $working_dir
-	git clone --recursive https://github.com/awslabs/amazon-kinesis-video-streams-producer-sdk-cpp.git
+    cmake_c_flags=$1
+    cmake_toolchain_flags=$2
+    cmake_includes=$3
+	#git clone --recursive https://github.com/awslabs/amazon-kinesis-video-streams-producer-sdk-cpp.git
 	mkdir -p amazon-kinesis-video-streams-producer-sdk-cpp/build
 	cd amazon-kinesis-video-streams-producer-sdk-cpp/build
-	cmake -DBUILD_GSTREAMER_PLUGIN=TRUE ..
-	if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+
+	echo "======== CMake run cmd: ========="
+	echo "cmake -DBUILD_OPENSSL_PLATFORM=linux-generic32 -DBUILD_LOG4CPLUS_HOST=arm-linux -DBUILD_GSTREAMER_PLUGIN=TRUE $cmake_toolchain_flags -DCMAKE_C_FLAGS='$cmake_c_flags' -DGST_APP_INCLUDE_DIRS='$cmake_includes' .."
+
+	cmake -DBUILD_OPENSSL_PLATFORM=linux-generic32 \
+	    -DBUILD_LOG4CPLUS_HOST=arm-linux \
+	    -DBUILD_GSTREAMER_PLUGIN=TRUE \
+	    $cmake_toolchain_flags \
+	    -DCMAKE_C_FLAGS="$cmake_c_flags" \
+	    -DCMAKE_CXX_FLAGS="$cmake_c_flags" \
+	    -DGST_APP_INCLUDE_DIRS="$cmake_includes" \
+	    ..
+	if [ $? -ne 0 ]; then exit 1; fi
 	make -j
-	if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+	if [ $? -ne 0 ]; then exit 1; fi
 # 	mkdir -p $ayla_install_dir/lib/kvsd
 # 	cp -av ../open-source/local/lib/* $ayla_install_dir/lib/kvsd
 # 	cp -av *.so $ayla_install_dir/lib/kvsd
 }
 
 install_kvsd_stream_webrtc() {
-	cd $working_dir
-	git clone --recursive https://github.com/awslabs/amazon-kinesis-video-streams-webrtc-sdk-c.git
+    cmake_c_flags=$1
+    cmake_toolchain_flags=$2
+    cmake_includes=$3
+    cmake_library_path=$4
+	#git clone --recursive https://github.com/awslabs/amazon-kinesis-video-streams-webrtc-sdk-c.git
 	mkdir -p amazon-kinesis-video-streams-webrtc-sdk-c/build
-	cd amazon-kinesis-video-streams-webrtc-sdk-c/
-	git checkout b820e2ea3d28c670da2fbc789face742cb949ef1
-	git apply $ayla_src_dir/kvs-webrtc-slave.diff
-	cd build
-	cmake ..
-	if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+	cd amazon-kinesis-video-streams-webrtc-sdk-c/build
+	#git checkout b820e2ea3d28c670da2fbc789face742cb949ef1
+	#git apply $ayla_src_dir/kvs-webrtc-slave.diff
+	cmake -DBUILD_OPENSSL=TRUE \
+	        -DBUILD_OPENSSL_PLATFORM=linux-generic32 \
+	        -DBUILD_LIBSRTP_HOST_PLATFORM=x86_64-unknown-linux-gnu \
+	        -DBUILD_LIBSRTP_DESTINATION_PLATFORM=arm-buildroot-linux-gnueabi \
+	        $cmake_toolchain_flags \
+            -DCMAKE_C_FLAGS="$cmake_c_flags" \
+            -DCMAKE_CXX_FLAGS="$cmake_c_flags" \
+            -DCMAKE_LIBRARY_PATH="$cmake_library_path" \
+            -DGST_APP_INCLUDE_DIRS="$cmake_includes" \
+            ..
+	if [ $? -ne 0 ]; then exit 1; fi
 	make -j
-	if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+	if [ $? -ne 0 ]; then exit 1; fi
 # 	cp -fv samples/kvsWebrtcClientMasterGstSample $ayla_install_dir/bin/kvsd_stream_webrtc
 }
 
 install_kvsd_stream_master() {
-    cd $working_dir
-    cp -avf $ayla_src_dir/kvsd_stream_master $working_dir
+    cmake_c_flags=$1
+    cmake_toolchain_flags=$2
     mkdir kvsd_stream_master/build
     cd kvsd_stream_master/build
-    cmake ..
-    if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+    cmake $cmake_toolchain_flags -DCMAKE_C_FLAGS="$cmake_c_flags" ..
+    if [ $? -ne 0 ]; then exit 1; fi
     make -j
-    if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+    if [ $? -ne 0 ]; then exit 1; fi
 #     cp -fv kvsd_stream_master $ayla_install_dir/bin
 }
 
 install_kvsd_stream_hls() {
-    cmake_defines=$1
-    cd $working_dir
-    cp -avf $ayla_src_dir/kvsd_stream_hls $working_dir
+    cmake_c_flags=$1
+    cmake_toolchain_flags=$2
     mkdir kvsd_stream_hls/build
     cd kvsd_stream_hls/build
-    cmake "$cmake_defines" ..
-    if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+    cmake $cmake_toolchain_flags -DCMAKE_C_FLAGS="$cmake_c_flags" ..
+    if [ $? -ne 0 ]; then exit 1; fi
     make -j
-    if [ $? -ne 0 ]; then print_msg "Failed."; exit 1; fi
+    if [ $? -ne 0 ]; then exit 1; fi
 #     cp -fv kvsd_stream_hls $ayla_install_dir/bin
 }
 
 #####################  Main
-if [[ $# != 3 ]]; then
-    echo "Usage: $0 <build_dir> <kvsd_src_dir> <cmake_defines>"
+if [ $# -ne 4 ]; then
+    help
     exit 1
 fi
 
-working_dir=$(realpath $1)
-shift 1
-ayla_src_dir=$(realpath $1)
-shift
-cmake_defines=$@
+cmake_c_flags=$(cat $1)
+cmake_toolchain_flags=$(cat $2)
+cmake_includes=$(cat $3)
+cmake_library_path=$(cat $4)
 
-echo "=== Working dir:   $working_dir"
-echo "=== Ayla src dir:  $ayla_src_dir"
-echo "=== Cmake defines: $cmake_defines"
+echo "=== Cmake C flags: $cmake_c_flags"
+echo "=== Cmake Toolchain flags: $cmake_toolchain_flags"
 
-#install_hls_sdk
-#install_kvsd_stream_master
-install_kvsd_stream_hls $cmake_defines
-#install_kvsd_stream_webrtc
+
+DIR=$PWD
+
+#cd $DIR
+#install_hls_sdk "$cmake_c_flags" "$cmake_toolchain_flags" "$cmake_includes"        # works
+#install_hls_sdk "$cmake_c_flags" "$cmake_toolchain_flags" "$cmake_includes" "$cmake_library_path" # not tested
+
+# cd $DIR
+# install_kvsd_stream_master "$cmake_c_flags" "$cmake_toolchain_flags"
+
+# cd $DIR
+# install_kvsd_stream_hls "$cmake_c_flags" "$cmake_toolchain_flags"
+
+cd $DIR
+install_kvsd_stream_webrtc "$cmake_c_flags" "$cmake_toolchain_flags" "$cmake_includes" "$cmake_library_path"
 
 exit 0
