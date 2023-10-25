231c871a326327382b32e833c658d3cd5405b85f
Maciej Andrzejewski <maciej.andrzejewski@digica.com>
2023-10-25 13:28:29 +0200

    Fix issue with HLS MAX_FRAGMENTED

--- kvsd_apps/kvsd_stream_master/main.c
+++ kvsd_apps/kvsd_stream_master/main.c
@@ -336,6 +336,7 @@ start_source()
     GstElement* videoconvert = gst_element_factory_make("videoconvert", "videoconvert"); GST_CHK_ELEM(videoconvert);
     GstElement* x264enc = gst_element_factory_make("x264enc", "x264enc"); GST_CHK_ELEM(x264enc);
     set_check_property(x264enc, "tune", 4 /*"zerolatency"*/, NULL);
+    set_check_property(x264enc, "key-int-max", 10, NULL);
     GstElement* rtph264pay = gst_element_factory_make("rtph264pay", "rtph264pay"); GST_CHK_ELEM(rtph264pay);
 
     tee = gst_element_factory_make("tee", "tee"); GST_CHK_ELEM(tee);
